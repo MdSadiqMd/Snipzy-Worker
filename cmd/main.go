637ffec556435d76a90c3efe66728cd97e6be0c3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-07-25 19:46:55 +0530

    ✅ snipzy worker done

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"github.com/MdSadiqMd/snipzy-worker/internal/handlers"
+	"github.com/MdSadiqMd/snipzy-worker/pkg/config"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	cfg := config.New()
+	handler := handlers.NewVideoHandler(cfg)
+	lambda.Start(handler.HandleClipRequest)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MdSadiqMd/snipzy-worker
 
 go 1.23.3
+
+require github.com/aws/aws-lambda-go v1.49.0
